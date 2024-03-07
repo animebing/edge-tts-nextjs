9f04511f0d90f587750751c70da1cea5f8bea759
Yanbing Dong <dongyanbing@metasota.ai>
2024-03-07 17:59:13 +0800

    react select

--- components/options/index.js
+++ components/options/index.js
@@ -1,12 +1,5 @@
 import Select from 'react-select';
-
-const voices = [
-  {value: 'zh-CN-XiaoxiaoNeural', label: 'zh-CN-XiaoxiaoNeural, Female'},
-  {value: 'zh-CN-YunxiNeural', label: 'zh-CN-YunxiNeural, Male'},
-  {value: 'en-US-AnaNeural', label: 'en-US-AnaNeural, Female'},
-  {value: 'en-US-AndrewNeural', label: 'en-US-AndrewNeural, Male'},
-];
-
+import { voices } from '@/lib/constants';
 
 export default ({
   voice,
