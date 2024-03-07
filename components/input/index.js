9d785926f6324ebc9b2ec365937107cc3cc5ec00
Yanbing Dong <dongyanbing@metasota.ai>
2024-03-07 17:59:23 +0800

    update

--- components/input/index.js
+++ components/input/index.js
@@ -1,10 +1,21 @@
-export default () => {
+export default ({
+  text,
+  setText,
+  setAudioUrl,
+}) => {
+  const changeHandler = (event) => {
+    setText(event.target.value);
+    setAudioUrl('');
+  };
+
   return (
     <div className="mt-4">
       <textarea 
         placeholder="type in text here"
         rows="10"
-        className="w-full text-lg p-2"
+        className="w-full text-lg p-2 border-2 border-solid rounded-md"
+        value={text}
+        onChange={changeHandler}
       >
       </textarea>
     </div>
