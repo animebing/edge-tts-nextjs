f5f4e40b0fccb2b10f739cee964894153cc18581
Yanbing Dong <dongyanbing@metasota.ai>
2024-03-07 17:58:36 +0800

    update

--- app/page.js
+++ app/page.js
@@ -6,28 +6,52 @@ import Hero from '@/components/hero'
 import Input from '@/components/input'
 import Options from '@/components/options'
 import { useState } from 'react';
+import { voices } from '@/lib/constants';
 
 export default function Home() {
-  const [voice, setVoice] = useState(null);
+  const [voice, setVoice] = useState(voices[0]);
   const [text, setText] = useState('');
   const [loading, setLoading] = useState(false);
-  const [modelUrl, setModelUrl] = useState('');
-  
+  const [audioUrl, setAudioUrl] = useState('');
+
   const clickHandler = async () => {
     setLoading(true);
+    setAudioUrl('')
 
-    
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: text,
+          voice: voice.value,
+        }),
+      });
 
-    setLoading(false);
+      if (!response.ok) {
+        throw new Error("failed to generate audio");
+      }
+
+      const data = await response.arrayBuffer();
+      const blob = new Blob([data], { type: "audio/mpeg" });
+      const audioUrl = URL.createObjectURL(blob);
+      setAudioUrl(audioUrl)
+    } catch (error) {
+      console.log("error in tts:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <main className="flex-col w-full px-8 py-8 md:max-w-3xl md:mx-auto">
+    <main className="h-screen w-full px-8 py-8 md:max-w-3xl md:mx-auto">
       <Hero />
       <Options voice={voice} setVoice={setVoice} />
-      <Input text={text} setText={setText} />
-      <Button loading={loading}/>
-      {modelUlr && <Audio modelUrl={modelUrl} />}
+      <Input text={text} setText={setText} setAudioUrl={setAudioUrl} />
+      <Button loading={loading} clickHandler={clickHandler} />
+      {audioUrl && <Audio audioUrl={audioUrl} />}
     </main>
   );
 }
