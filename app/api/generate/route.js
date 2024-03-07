69c96f665617bc48937c755f96455f8bbc2374f5
Yanbing Dong <dongyanbing@metasota.ai>
2024-03-07 17:57:42 +0800

    tts api

---
+++ app/api/generate/route.js
@@ -0,0 +1,22 @@
+export async function POST(request) {
+  const requestBody = await request.json();
+  const response = await fetch(process.env.EDGE_TTS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  })
+  
+  if (!response.ok) {
+    throw new Error("tts-edge failed");
+  }
+
+  const audioData = await response.arrayBuffer();
+  
+  return new Response(audioData, {
+    headers: {
+      "Content-Type": "audio/mpeg",
+    }
+  });
+}
